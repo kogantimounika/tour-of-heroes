798d4a3c9a04fd8dd409e354ea0e70acec933cfa
kogantimounika <54623414+kogantimounika@users.noreply.github.com>
2020-10-01 23:59:32 -0700

    Added mock data to the hero component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,17 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mahesh' },
+  { id: 12, name: 'NTR' },
+  { id: 13, name: 'Surya' },
+  { id: 14, name: 'Charan' },
+  { id: 15, name: 'Prabhas' },
+  { id: 16, name: 'Ram' },
+  { id: 17, name: 'Dulkar' },
+  { id: 18, name: 'Pavan Kalyan' },
+  { id: 19, name: 'Manoj' },
+  { id: 20, name: 'Vijay' }
+];
+
+
+
